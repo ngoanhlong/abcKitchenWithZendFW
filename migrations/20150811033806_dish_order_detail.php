c265f72086dafa8d502dbe3323652958695a9596
Ngo Anh Long <ngoanhlong@gmail.com>
2015-08-11 10:45:28 +0700

    Completed database

--- migrations/20150811033806_dish_order_detail.php
+++ migrations/20150811033806_dish_order_detail.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class DishMenuDetail extends AbstractMigration
+class DishOrderDetail extends AbstractMigration
 {
     /**
      * Change Method.
@@ -14,16 +14,15 @@ class DishMenuDetail extends AbstractMigration
      */
     public function change()
     {
-        $table = $this->table('dishMenuDetail', ['id'=>false]);
+        $table = $this->table('dishOrderDetail', ['id'=>false]);
         $table
+                ->addColumn('userName', 'string', ['limit' => 32])
                 ->addColumn('ID', 'integer', ['limit' => 8])
-                ->addColumn('menuCreatedBy', 'string', ['limit' => 32])
                 ->addColumn('menuForDate', 'date')
                 ->addColumn('dishNumber', 'integer', ['limit' => 8])
                 ->addColumn('dishName', 'string', ['limit'=>32])
                 ->addColumn('price', 'integer', ['limit' => 12])
                 ->addColumn('description', 'string', ['limit' => 1000])
-                ->addColumn('userName', 'string', ['limit' => 32])
                 ->addColumn('imageName', 'string', ['limit' => 32])
                 ->create();
         
